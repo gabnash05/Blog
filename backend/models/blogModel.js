20cbc711724c0759e573a3118f58f382dea56907
kimgab <nasayaokim@gmail.com>
2024-02-25 22:33:01 +0800

    create blog and user model

---
+++ backend/models/blogModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const BlogSchema = new Schema({
+  author: {
+    type: String,
+    required: true,
+  },
+  authorProfilePic: {
+    type: Image,
+    required: false
+  }, 
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  }, 
+  file: {
+    type: [],
+  }
+}, {timestamps: true});
+
+
+const Blogs = mongoose.model('Blogs', BlogSchema);
+
+export default Blogs;
---
+++ backend/models/userModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }, 
+  password: {
+    type: String,
+    required: true
+  }
+});
